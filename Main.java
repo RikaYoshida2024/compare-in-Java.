1b42b20b2fd51b8386bfa464fd0dc1a949559cec
RikaYoshida2024 <spandanpatnaik123@gmail.com>
2024-11-25 21:23:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Main{
+    public static void main(String[] args){
+        String[] cars = {"Volvo", "BMW", "Porsche"};
+        String[] cars2 = {"Volvo", "BMW", "Porsche"};
+        System.out.println(Arrays.compare(cars,cars2));
+    }
+}
